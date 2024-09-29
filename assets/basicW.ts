9d892c121d788821762939ab5e3bd54417ba2537
Zuohuang Cai <zc26226@gmail.com>
2024-09-29 23:36:39 +0200

    update

--- assets/basicW.ts
+++ assets/basicW.ts
@@ -24,6 +24,61 @@ export const basicW = {
             ]
         },
         {
+            "english": "capital",
+            "chinese": [
+                "首都",
+                "大写字母"
+            ]
+        }, {
+            "english": "valley",
+            "chinese": [
+                "山谷",
+                "流域"
+            ]
+        },
+        {
+            "english": "coast",
+            "chinese": [
+                "海岸",
+                "沿海"
+            ]
+        },
+        {
+            "english": "semester",
+            "chinese": [
+                "学期",
+                "半年"
+            ]
+        },
+        {
+            "english": "definition",
+            "chinese": [
+                "定义",
+                "释义"
+            ]
+        },
+        {
+            "english": "abroad",
+            "chinese": [
+                "国外",
+                "海外"
+            ]
+        },
+        {
+            "english": "sentence",
+            "chinese": [
+                "句子",
+                "判决"
+            ]
+        },
+        {
+            "english": "digestion",
+            "chinese": [
+                "消化",
+                "领会"
+            ]
+        },
+        {
             "english": "native",
             "chinese": [
                 "纯粹",
